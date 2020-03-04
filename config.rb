4585cb0bdf88979d928f9ff1c5209bc922ab576c
Harrison Broadbent <harrisonbroadbent@gmail.com>
2020-03-04 13:57:57 +1100

    Move page files to root directory
    - Move pages files to root and remove /pages folder
    - Re-write menu partial to filter out folders and index file
    - Remove pretty-urls extension as Netlify can do that on the server side

--- config.rb
+++ config.rb
@@ -1,7 +1,6 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :directory_indexes
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
